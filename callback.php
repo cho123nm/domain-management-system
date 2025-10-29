8a8495594e617a935458a89e5b54dda686253709
cho123nm <chumlongchinhgiua@gchinhgiuagmai.com>
2025-10-29 15:43:02 +0700

    Update comments in callback.php

--- callback.php
+++ callback.php
@@ -11,28 +11,28 @@ require_once(__DIR__.'/Repositories/UserRepository.php');
 # Status == 200 -> thẻ đúng
 # Status == 201 -> thẻ sai mệnh giá
 # Status == 100 -> thẻ sai
-# Quý khách lưu ý dựa vào status để trả lại kết quả cho khách
+# lưu ý dựa vào status để trả lại kết quả cho khách hàng
 $status = $_GET['status']; 
 
-# Đối số pricesvalue là mệnh giá thẻ cào lúc quý khách gửi thẻ
+# Đối số pricesvalue là mệnh giá thẻ cào lúc khách gửi thẻ
 $pricesvalue = $_GET['pricesvalue'];
 
-# Đối số value_receive là mệnh giá thực của thẻ, quý khách chỉ quan tâm đối số này khi status nhận giá trị 200 hoặc 201
+# Đối số value_receive là mệnh giá thực của thẻ,  chỉ quan tâm đối số này khi status nhận giá trị 200 hoặc 201
 $value_receive = $_GET['value_receive'];
 
-# Đối số card_code là mã thẻ cào quý khách gửi
+# Đối số card_code là mã thẻ cào khách hàng gửi
 $card_code = $_GET['card_code'];
 
-# Đối số card_seri là serial cào quý khách gửi
+# Đối số card_seri là serial cào khách hàng gửi
 $card_seri = $_GET['card_seri'];
 
-# Đối số value_customer_receive là mệnh giá mà quý khách nhận được sau khi đã trừ chiết khấu, mệnh giá này là mệnh giá được cộng vào tài khoản CARDVIP
+# Đối số value_customer_receive là mệnh giá mà khách hàng nhận được sau khi đã trừ chiết khấu, mệnh giá này là mệnh giá được cộng vào tài khoản CARDVIP
 $value_customer_receive = $_GET['value_customer_receive'];
 
-# Đối số requestid là mã đơn thẻ cào mà quý khách đã gửi sang
+# Đối số requestid là mã đơn thẻ cào mà khách hàng đã gửi sang
 $requestid = $_GET['requestid'];
 
-# Quý khách check thẻ cào có tồn tại trong hệ thống hoặc thẻ cào đó đã xử lý chưa thông qua các điều kiện như card_code, card_seri, requestid, trạng thái xử lý của thẻ
+# khách hàng kiểm tra thẻ cào có tồn tại trong hệ thống hoặc thẻ cào đó đã xử lý chưa thông qua các điều kiện như card_code, card_seri, requestid, trạng thái xử lý của thẻ
 # Sau đó sẽ check trạng thái của biến $status để cập nhật trạng thái của thẻ theo hướng dẫn trên.
 $cardRepo = new CardRepository($connect);
 $userRepo = new UserRepository($connect);
